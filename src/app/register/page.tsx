086a0132063afc4ff99a5a6383d2c953c39f8c3f
Kaleb <kalebjsmith@yahoo.com>
2024-05-30 20:38:46 -0500

    Create Route for Auth page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
+import axios from "axios"
 import Input from "../components/input/Input"
-import { useState } from "react"
+import { FormEvent, useState } from "react"
+import router from "next/router"
 
 
 interface initialStateProps {
@@ -18,7 +20,6 @@ const initialState:initialStateProps = {
 }
 
 
-
 export default function page() {
     const [state, setState] = useState(initialState)
 
@@ -26,13 +27,33 @@ export default function page() {
         setState({...state, [e.target.name]: e.target.value})
     }
 
+    const onSubmit = (event:FormEvent) => {
+        event.preventDefault()
+        axios.post('/api/register', state)
+        .then(()=> {
+            router.reload()
+        })
+        .then(() => {
+            setTimeout(() => {
+                router.push('/login')
+            }, 2500)
+        })
+        .catch((err:any) => {
+            console.log(err)
+        })
+        .finally(() => {
+
+        });
+    }
+
     return (
         <div className="registerWrapper">
-            <form action="" className="formBox">
+            <form action="" className="formBox" onSubmit={onSubmit}>
                 <div className="innerForm">
                     <Input placeholder="Full Name" name='name' id="name" type='text' onChange={handleChange} value={state.name} />
                     <Input placeholder="Email" name='email' id="email" type='email' onChange={handleChange} value={state.email} />
                     <Input placeholder="Password" name='password' id="password" type='password' onChange={handleChange} value={state.password} />
+                    <button type="submit">Sign in</button>
                 </div>
             </form>
         </div>
